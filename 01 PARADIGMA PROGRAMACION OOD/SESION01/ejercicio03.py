a8327f2577efc57e1c98ed95452fcaf02aacb0ef
Sebastian Aviña <sebastian@rectificacionesavina.com>
2024-05-07 07:14:40 -0600

    Ejercicios de la sesion 01

---
+++ 01 PARADIGMA PROGRAMACION OOD/SESION01/ejercicio03.py
@@ -0,0 +1,7 @@
+base = float(input("Ingresa la base"))
+altura = float(input("Ingresa la altura"))
+
+area = base * altura
+
+print("El area es: ", area)
+
---
+++ 01 PARADIGMA PROGRAMACION OOD/SESION01/ejercicio04.py
@@ -0,0 +1,10 @@
+import math
+
+diametro = float(input("Ingresa el diametro"))
+
+radio = diametro/2
+
+area = math.pi  * (radio*radio)
+
+print("El area es: ", area)
+
